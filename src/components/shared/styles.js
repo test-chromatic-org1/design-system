8b15dfaeec04868de0e195be5687d0d9ac345317
domyen <dom@hichroma.com>
2020-04-20 17:54:10 -0400

    Update styles.js

--- src/components/shared/styles.js
+++ src/components/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: 'blue', // coral
+  primary: 'green', // coral
   secondary: '#1EA7FD', // ocean
   tertiary: '#DDDDDD',
 
